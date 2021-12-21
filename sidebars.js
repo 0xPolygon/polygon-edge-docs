97f7321fe896d69eca7845680d03131bcf372701
AleksaOpacic <aleksaopacic988@gmail.com>
2021-12-21 12:49:15 +0100

    Add JSON RPC doc

--- sidebars.js
+++ sidebars.js
@@ -8,6 +8,7 @@ module.exports = {
                 'get-started/set-up-ibft-locally',
                 'get-started/set-up-ibft-on-the-cloud',
                 'get-started/cli-commands',
+                'get-started/json-rpc-commands',
             ]
         },
         {
